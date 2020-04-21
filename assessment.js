d307e0ea3c6fc064b94a969210ee9472fea1967b
suika <51790754+suika-p@users.noreply.github.com>
2020-04-21 21:40:35 +0900

    #1 に対する対応を行いました

--- assessment.js
+++ assessment.js
@@ -52,6 +52,7 @@ assessmentButton.onclick = () => {
     script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
     tweetDivided.appendChild(script);
 };
+// 修正
 const answers = [
   '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
